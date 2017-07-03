5b0e096b8476fe6e9b156282b8dc774436a3a4a8
saurav8112 <saurav.verma8112@gmail.com>
2017-07-03 11:02:23 +0530

    pagination with left right button: commit-5

--- js/components/tableViewComponent.js
+++ js/components/tableViewComponent.js
@@ -10,17 +10,23 @@ angular.module('SNS').component('tableView', {
         this.createNameKey = function(key) {
             return key.replace('_', " ").replace('.', ' ');
         }
-
+        this.currentPage = 1
         this.range = {
             start: 1,
             end: 10,
             viewPerPage: 10
+        }
+        this.viewVal = this.range.viewPerPage
+        this.viewPerPageChanged = function() {
+            this.range.viewPerPage =  this.viewVal;
+            this.goToPage(1);
         }
 
         this.goToPage = function(page) {
             page = Math.ceil(page);
             this.range.start = (this.range.viewPerPage * page) - this.range.viewPerPage;
             this.range.end = (this.range.viewPerPage * page);
+            this.currentPage = page
         }
 
         this.performAction = function(action, item) {
